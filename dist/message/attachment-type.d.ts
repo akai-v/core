94348d99322a7291a9ce614eacd2441c98026ee9
storycraft <storycraft@storyboard.ml>
2019-10-08 23:35:54 +0900

    remove compiled file

--- dist/message/attachment-type.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum AttachmentType {
-    IMAGE = 0,
-    AUDIO = 1,
-    VIDEO = 2,
-    FILE = 3
-}
--- dist/module/command/parser/argument-parser.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ArgumentParser<T> {
-    parse(rawArgs: string): T;
-}
-export declare class SpaceSplitedParser implements ArgumentParser<string[]> {
-    parse(rawArgs: string): string[];
-}
-export declare class OptionParser implements ArgumentParser<Map<string, any>> {
-    parse(rawArgs: string): Map<string, any>;
-}
